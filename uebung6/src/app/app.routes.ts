52fde9360c9c7317829388d724e0a7a616a80385
Kata <Katharina.Eichmann@student.htw-berlin.de>
2026-06-09 15:27:50 +0200

    Uebung 6 angefangen

--- uebung6/src/app/app.routes.ts
+++ uebung6/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Form } from './form/form';
+import { Table } from './table/table';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home }, 
+    { path: 'create', component: Form },
+    { path: 'read', component: Table }
+];
--- uebung6/src/app/app.ts
+++ uebung6/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from './nav/nav';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Nav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
